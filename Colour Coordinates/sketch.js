9dcf628daf5a1a61520aa99badd805b0bcd8ec1f
Jake Warren <jakewarren@pc-138-172.customer.dairy.vic.lan>
2017-11-26 22:30:47 +0000

    Files Renamed

---
+++ Colour Coordinates/sketch.js
@@ -0,0 +1,27 @@
+//https://p5js.org
+//https://p5js.org/learn/
+
+//DAT405 / GAD405
+//00_Template
+//The purpose of this sketch is to provide a basic template
+//for P5.js projects
+
+//Initialization function
+function setup() {
+  //Print a message to the console - to view using Chrome:
+  //View > Developer > Developer Tools > Console
+  console.log("Initialization: OK")
+  createCanvas(200,500);
+  createBorder(5,8)
+}
+
+//Rendering function
+function draw() {
+  //Set a background color
+  background(60,200,105);
+
+  //Print a message to the console - to view using Chrome:
+  //View > Developer > Developer Tools > Console
+  console.log("Rendering...")
+  noLoop();
+}
